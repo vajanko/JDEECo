a12e574671fd686d602bf63381afe156b145463e
Ondrej Kováč <vajanko@gmail.com>
2014-11-04 09:26:02 +0100

    Added helper methods to ensemble base class which allows for checking component roles.

--- jdeeco-connectors/src/cz/cuni/mff/d3s/deeco/connectors/EnsembleBase.java
+++ jdeeco-connectors/src/cz/cuni/mff/d3s/deeco/connectors/EnsembleBase.java
@@ -1,10 +1,14 @@
 package cz.cuni.mff.d3s.deeco.connectors;
 
+import java.util.Collection;
 import java.util.List;
 
 public abstract class EnsembleBase {
 	
-	public static Boolean matchRole(String role, List<String> cRoles, List<String> mRoles) {
-		return cRoles != null && mRoles != null && cRoles.contains(role) && mRoles.contains(role);
+	public static Boolean matchRole(String role, Collection<String> cRoles, Collection<String> mRoles) {
+		return matchRole(role, cRoles) && matchRole(role, mRoles);
+	}
+	public static Boolean matchRole(String role, Collection<String> roles) {
+		return roles != null && roles.contains(role);
 	}
 }
