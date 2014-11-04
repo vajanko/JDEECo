0ca0aa2cbb94229f04461163d5fb40e1e2c6197b
Ondrej Kováč <vajanko@gmail.com>
2014-11-04 09:27:15 +0100

    Base class of ensemble component has a list of relays (strings).

--- jdeeco-connectors/src/cz/cuni/mff/d3s/deeco/connectors/EnsembleComponent.java
+++ jdeeco-connectors/src/cz/cuni/mff/d3s/deeco/connectors/EnsembleComponent.java
@@ -7,15 +7,23 @@ public abstract class EnsembleComponent {
 	
 	public String id;
 	public List<String> roles;
+	public List<String> relays;
 	
 	public Boolean hasRole(String role) {
 		return this.roles.contains(role);
 	}
 	
+	protected void addRole(String role) {
+		this.roles.add(role);
+	}
+	
 	public EnsembleComponent(String id) {
 		this.roles = new ArrayList<String>();
+		this.relays = new ArrayList<String>();
 		this.id = id;
-		this.roles.add(this.getClass().getSimpleName());
+		addRole(this.getClass().getSimpleName());
+	
+		// TODO: add implemented interfaces of this instance
 	}
 }
 
