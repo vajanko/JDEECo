8e926c90cf93a6e272f7c4b52b2e189d13c3efd3
Ondrej Kováč <vajanko@gmail.com>
2014-11-16 10:36:24 +0100

    Added IPTable class encapsulating collection of IP addresses known by a particular host.

---
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/network/connector/IPTable.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package cz.cuni.mff.d3s.deeco.network.connector;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Encapsulates a collection of IP addresses known by a particular host.
+ * 
+ * @author Ondrej Kováč <info@vajanko.me>
+ */
+public class IPTable {
+	private Set<String> register;
+	
+	/**
+	 * Add given IP address to the current collection if it does not contain yet.
+	 * If IP address already exists in the collection it won't be affected. 
+	 * @param address IP address to be added.
+	 */
+	public void addAddress(String address) {
+		register.add(address);
+	}
+	public void removeAddress(String address) {
+		register.remove(address);
+	}
+	public Collection<String> getAddresses() {
+		return register;
+	}
+	
+	public IPTable() {
+		this.register = new HashSet<String>(); 
+	}
+}
