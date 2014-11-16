c15f4ae0e50cdc190dc8487fcc0c5a2eef9dd8e6
Ondrej Kováč <vajanko@gmail.com>
2014-11-16 11:25:59 +0100

    Added simple implementation of IPGossipStrategy using an IPController. As IPController provides a table of IP addresses, IPGossip strategy is very simple - it is only a wrapper of IPTable provided by IPController.

---
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/network/ip/IPGossipClientStrategy.java
@@ -0,0 +1,42 @@
+package cz.cuni.mff.d3s.deeco.network.ip;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import cz.cuni.mff.d3s.deeco.knowledge.KnowledgeManager;
+import cz.cuni.mff.d3s.deeco.knowledge.ValueSet;
+import cz.cuni.mff.d3s.deeco.logging.Log;
+import cz.cuni.mff.d3s.deeco.model.runtime.api.KnowledgePath;
+import cz.cuni.mff.d3s.deeco.network.AbstractHost;
+import cz.cuni.mff.d3s.deeco.network.DataReceiver;
+import cz.cuni.mff.d3s.deeco.network.DataSender;
+import cz.cuni.mff.d3s.deeco.network.IPGossipStrategy;
+import cz.cuni.mff.d3s.deeco.network.KnowledgeData;
+
+/**
+ * Provides list of recipient IP addresses for given knowledge data and sender.
+ * 
+ * @author Ondrej Kováč <info@vajanko.me>
+ */
+public class IPGossipClientStrategy implements IPGossipStrategy {
+	
+	private IPTable ipTable;
+	
+	/* (non-Javadoc)
+	 * @see cz.cuni.mff.d3s.deeco.network.IPGossipStrategy#getRecipients(cz.cuni.mff.d3s.deeco.network.KnowledgeData, cz.cuni.mff.d3s.deeco.knowledge.KnowledgeManager)
+	 */
+	@Override
+	public Collection<String> getRecipients(KnowledgeData data, KnowledgeManager sender) {
+		
+		ArrayList<String> res = new ArrayList<String>(ipTable.getAddresses());
+		res.remove(sender.getId());
+		return res;
+	}
+	
+	public IPGossipClientStrategy(IPController controller) {
+		controller.getIPTable();
+	}
+}
