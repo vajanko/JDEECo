af96603dc91c99468d01fdb0fb146e0d3a5fdffd
Ondrej Kováč <vajanko@gmail.com>
2014-11-18 09:03:04 +0100

    Added possible extensions of GenericDataReceiver (in comments).

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/network/GenericDataReceiver.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/network/GenericDataReceiver.java
@@ -8,8 +8,20 @@ package cz.cuni.mff.d3s.deeco.network;
  * @author Ondrej Kováč <info@vajanko.me>
  */
 public interface GenericDataReceiver<TData> {
+	
+	// this method actually handles already deserialised object
 	public void receive(TData obj);
 	
+
+	/**
+	 * Gets {@link DataReceiver} implementation used to receive serialised objects from
+	 * the network. {@link DataReceiver} receives deserialised object from the {@link PacketReceiver}
+	 * and it should be passed to {@link receive} method if it is of type {@link TData}. 
+	 * 
+	 * @return Instance of {@link DataReceiver}
+	 */
 	// TODO: there should be method like this
+	// but notice that an object capable of processing TData must not be able of receiving it
+	// from packet receiver
 	//public DataReceiver getDataReceiver();
 }
