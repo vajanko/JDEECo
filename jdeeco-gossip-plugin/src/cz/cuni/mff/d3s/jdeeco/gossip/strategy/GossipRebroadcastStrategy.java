606921e675ba1ce14e0a5dfa73a6b292a2b9853a
Ondrej Kováč <vajanko@gmail.com>
2015-03-23 16:03:33 +0100

    Fixed rebroadcast strategy. Checking whether the received knowledge is not local and older than the current one present on the node.

--- jdeeco-gossip-plugin/src/cz/cuni/mff/d3s/jdeeco/gossip/strategy/GossipRebroadcastStrategy.java
+++ jdeeco-gossip-plugin/src/cz/cuni/mff/d3s/jdeeco/gossip/strategy/GossipRebroadcastStrategy.java
@@ -4,13 +4,17 @@
 package cz.cuni.mff.d3s.jdeeco.gossip.strategy;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import cz.cuni.mff.d3s.deeco.network.KnowledgeData;
+import cz.cuni.mff.d3s.deeco.network.KnowledgeMetaData;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoContainer;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoPlugin;
 import cz.cuni.mff.d3s.jdeeco.gossip.GossipProperties;
+import cz.cuni.mff.d3s.jdeeco.gossip.KnowledgeProvider;
 import cz.cuni.mff.d3s.jdeeco.gossip.buffer.ReceptionBuffer;
 import cz.cuni.mff.d3s.jdeeco.network.Network;
 import cz.cuni.mff.d3s.jdeeco.network.address.MANETBroadcastAddress;
@@ -31,6 +35,9 @@ public class GossipRebroadcastStrategy implements L2Strategy, DEECoPlugin {
 	private double probability;
 	private Layer2 networkLayer;
 	private ReceptionBuffer messageBuffer;
+	private KnowledgeProvider knowledgeProvider;
+	
+	private Map<String, Long> currentVersions = new HashMap<String, Long>();
 	
 	/* (non-Javadoc)
 	 * @see cz.cuni.mff.d3s.jdeeco.network.l2.L2Strategy#processL2Packet(cz.cuni.mff.d3s.jdeeco.network.l2.L2Packet)
@@ -40,13 +47,27 @@ public class GossipRebroadcastStrategy implements L2Strategy, DEECoPlugin {
 		
 		if (packet.header.type.equals(L2PacketType.KNOWLEDGE)) {
 			KnowledgeData kd = (KnowledgeData)packet.getObject();
-			if (messageBuffer.getPulledTag(kd.getMetaData().componentId)) {
+			KnowledgeMetaData meta = kd.getMetaData();
+			if (messageBuffer.getPulledTag(meta.componentId)) {
 				// if knowledge was pulled it will be rebroadcasted
 				networkLayer.sendL2Packet(packet, MANETBroadcastAddress.BROADCAST);
-				messageBuffer.clearPulledTag(kd.getMetaData().componentId);
+				messageBuffer.clearPulledTag(meta.componentId);
 			}
-			else if (generator.nextDouble() < probability) {
-				networkLayer.sendL2Packet(packet, MANETBroadcastAddress.BROADCAST);
+			else {
+				// do not rebroadcast when receive own local knowledge
+				if (knowledgeProvider.hasLocal(meta.componentId))
+					return;
+				
+				// do not rebroadcast older versions than currently available
+				Long curVer = currentVersions.get(meta.componentId);
+				if (curVer != null && curVer >= meta.versionId)
+					return;
+				
+				currentVersions.put(meta.componentId, meta.versionId);
+				
+				if (generator.nextDouble() < probability) {
+					networkLayer.sendL2Packet(packet, MANETBroadcastAddress.BROADCAST);
+				}
 			}
 		}
 	}
@@ -56,13 +77,14 @@ public class GossipRebroadcastStrategy implements L2Strategy, DEECoPlugin {
 	 */
 	@Override
 	public List<Class<? extends DEECoPlugin>> getDependencies() {
-		return Arrays.asList(Network.class, ReceptionBuffer.class);
+		return Arrays.asList(Network.class, ReceptionBuffer.class, KnowledgeProvider.class);
 	}
 	/* (non-Javadoc)
 	 * @see cz.cuni.mff.d3s.deeco.runtime.DEECoPlugin#init(cz.cuni.mff.d3s.deeco.runtime.DEECoContainer)
 	 */
 	@Override
 	public void init(DEECoContainer container) {
+		this.knowledgeProvider = container.getPluginInstance(KnowledgeProvider.class);
 		this.messageBuffer = container.getPluginInstance(ReceptionBuffer.class);
 		this.networkLayer = container.getPluginInstance(Network.class).getL2();
 		// register L2 strategy
