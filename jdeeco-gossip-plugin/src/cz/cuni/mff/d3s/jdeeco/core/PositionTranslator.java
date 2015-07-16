54f142c0e6949a1b9d11b73776de9898ce9a3b7d
Ondrej Kováč <vajanko@gmail.com>
2015-07-16 17:24:48 +0200

    Added comments to PositionTranslator.

--- jdeeco-gossip-plugin/src/cz/cuni/mff/d3s/jdeeco/core/PositionTranslator.java
+++ jdeeco-gossip-plugin/src/cz/cuni/mff/d3s/jdeeco/core/PositionTranslator.java
@@ -9,5 +9,11 @@ package cz.cuni.mff.d3s.jdeeco.core;
  * @author Ondrej Kov·Ë <info@vajanko.me>
  */
 public interface PositionTranslator {
+	/**
+	 * Translates given position from one system to another.
+	 * 
+	 * @param pos Position to be translated into another positioning system.
+	 * @return Translated position.
+	 */
 	Position translate(Position pos);
 }
